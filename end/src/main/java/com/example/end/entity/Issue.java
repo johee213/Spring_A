7d7bd8f84c643d8e3ac4b1da2c165456017853e1
johee213 <johee0111@gmail.com>
2026-06-12 18:21:04 +0900

    Add files via upload

---
+++ end/src/main/java/com/example/end/entity/Issue.java
@@ -0,0 +1,15 @@
+package com.example.end.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Issue {
+
+    .
+
+
+}
---
+++ end/src/main/java/com/example/end/entity/Solution.java
@@ -0,0 +1,5 @@
+package com.example.end.entity;
+
+
+public class Solution {
+}
